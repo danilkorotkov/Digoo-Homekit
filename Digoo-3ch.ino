ed3f44b8fb11292c9673da85c9ce36daacf06d61
danilkorotkov <spballiance@gmail.com>
2021-03-13 02:00:12 +0300

    Add files via upload

---
+++ Digoo-3ch.ino
@@ -0,0 +1,126 @@
+#include "HomeSpan.h" 
+#include "DigooTH.h"
+
+#include <homeGW.h>
+#include <digoo.h>
+
+DigooData s_ch1;
+DigooData s_ch2;
+DigooData s_ch3;
+DigooData* s_ch0;
+
+HomeGW gw(1); // 1 is the number of plugins to be registered 
+digoo DigooStation;
+uint64_t prev_p = 0;
+uint8_t current_ch = 0;
+#define RF_RECEIVER_PIN 22 // D2
+
+void setup() {
+  Serial.begin(115200);
+  homespanInit();
+  while (!homeSpan.connected){
+    homeSpan.poll();
+    }
+  gw.setup(RF_RECEIVER_PIN);
+  gw.registerPlugin(&DigooStation); 
+
+  
+
+}
+
+void homespanInit(){
+  homeSpan.setApSSID("Digoo-AP");
+  homeSpan.setApPassword("");
+  homeSpan.setControlPin(0);
+  homeSpan.setStatusPin(2);
+  homeSpan.setLogLevel(1);
+
+  homeSpan.setSketchVersion("0.0.1");
+  //homeSpan.enableOTA();
+  
+  homeSpan.begin(Category::Sensors,"Digoo");
+  ///channel 1
+  new SpanAccessory(); 
+  
+    new Service::AccessoryInformation(); 
+      new Characteristic::Name("Digoo ch1 sensor"); 
+      new Characteristic::Manufacturer("Danil"); 
+      new Characteristic::SerialNumber("0000001"); 
+      new Characteristic::Model("ch1"); 
+      new Characteristic::FirmwareRevision("0.0.1"); 
+      new Characteristic::Identify();            
+      
+    new Service::HAPProtocolInformation();      
+      new Characteristic::Version("1.1.0"); 
+  
+    new Digoo(&s_ch1);  
+  
+  ///channel 2
+  new SpanAccessory(); 
+  
+    new Service::AccessoryInformation(); 
+      new Characteristic::Name("Digoo ch2 sensor"); 
+      new Characteristic::Manufacturer("Danil"); 
+      new Characteristic::SerialNumber("0000001"); 
+      new Characteristic::Model("ch2"); 
+      new Characteristic::FirmwareRevision("0.0.1"); 
+      new Characteristic::Identify();            
+      
+    new Service::HAPProtocolInformation();      
+      new Characteristic::Version("1.1.0"); 
+  
+    new Digoo(&s_ch2);
+  ///channel 3
+  new SpanAccessory(); 
+  
+    new Service::AccessoryInformation(); 
+      new Characteristic::Name("Digoo ch3 sensor"); 
+      new Characteristic::Manufacturer("Danil"); 
+      new Characteristic::SerialNumber("0000001"); 
+      new Characteristic::Model("ch3"); 
+      new Characteristic::FirmwareRevision("0.0.1"); 
+      new Characteristic::Identify();            
+      
+    new Service::HAPProtocolInformation();      
+      new Characteristic::Version("1.1.0"); 
+  
+    new Digoo(&s_ch3);
+}
+
+void loop() {
+  uint64_t p = 0;
+  homeSpan.poll();
+  gpio_intr_enable(gpio_num_t(RF_RECEIVER_PIN));
+  if(DigooStation.available()) 
+    if((p = DigooStation.getPacket())) {
+      if(p == prev_p) {
+        LOG1("smth catched\n");
+        current_ch = DigooStation.getChannel(p);
+        switch (current_ch) {
+          case 1:
+            s_ch0 = &s_ch1;
+            LOG1("ch1 ");
+            break;
+          case 2:
+            s_ch0 = &s_ch2;
+            LOG1("ch2 ");
+            break;
+          case 3:
+            s_ch0 = &s_ch3;
+            LOG1("ch3 ");
+            break;
+        }
+        s_ch0 ->batt        = !DigooStation.getBattery(p);
+        LOG1(s_ch0 ->batt);LOG1(" ");
+        s_ch0 ->temperature = DigooStation.getTemperature(p);
+        LOG1(s_ch0 ->temperature);LOG1(" ");
+        s_ch0 ->humidity    = (double)DigooStation.getHumidity(p);
+        LOG1(s_ch0 ->humidity);LOG1(" \n");
+        s_ch0 ->updated     = millis();
+        s_ch0 ->isNew[0]    = true;
+        s_ch0 ->isNew[1]    = true;
+        p = 0;
+      }
+      prev_p = p;
+    }
+}
---
+++ DigooTH.h
@@ -0,0 +1,92 @@
+uint32_t activeTimeout = 5*60*1000 ; //5мин
+struct DigooData{
+  uint32_t updated    = 0;
+  float   temperature = 0;
+  double humidity     = 100;
+  uint8_t batt        = 0;
+  bool    isNew[2]    = {true, true};
+  
+};
+
+struct Digoo : Service::TemperatureSensor {         // First we create a derived class from the HomeSpan 
+  
+  SpanCharacteristic *CurrentTemperature;              // here we create a generic pointer to a SpanCharacteristic 
+  SpanCharacteristic *StatusActive;
+  SpanCharacteristic *StatusLowBattery;
+  DigooData* Data;
+  
+  Digoo(DigooData* Data) : Service::TemperatureSensor(){
+ 
+    LOG1("Constructing digoo…\n");
+    this->Data = Data;
+    CurrentTemperature=new Characteristic::CurrentTemperature(25);// this is where we create the On Characterstic we had previously defined in setup().  Save this in the pointer created above, for use below
+    new SpanRange(-50, 100, 1);
+    StatusActive=new Characteristic::StatusActive(true);
+    StatusLowBattery=new Characteristic::StatusLowBattery(1);
+    
+    LOG1(this->Data->humidity);         
+    LOG1("Constructing Digoo successful!\n");
+
+    new DigooH(this->Data);
+    
+  } // end constructor
+
+
+  void loop(){                                     // loop() method
+    if ( (millis() - Data->updated) > activeTimeout ){
+      LOG1("sensor timeout\n");
+      if (StatusActive -> getVal()) {StatusActive->setVal(false);}
+      return;
+    }
+    
+    if (Data->isNew[0]){
+      LOG1("sensor update\n");
+      Data->isNew[0] = false;
+      
+      if (CurrentTemperature->getVal() != Data->temperature) {CurrentTemperature->setVal(Data->temperature);}
+      //CurrentTemperature->setVal(Data->temperature);
+      if (StatusLowBattery->getVal() != Data->batt) {StatusLowBattery->setVal(Data->batt);}
+
+      if ( !(StatusActive -> getVal()) ) {StatusActive->setVal(true);}
+    }
+  } // loop 
+
+
+  struct DigooH : Service::HumiditySensor {         // First we create a derived class from the HomeSpan 
+    
+    SpanCharacteristic *CurrentRelativeHumidity;              // here we create a generic pointer to a SpanCharacteristic 
+    SpanCharacteristic *StatusActive;
+    SpanCharacteristic *StatusLowBattery;
+    DigooData* DataH;
+    
+    DigooH(DigooData* Data) : Service::HumiditySensor(){
+   
+      LOG1("Constructing digooH…\n");
+      this->DataH = Data;
+      CurrentRelativeHumidity=new Characteristic::CurrentRelativeHumidity(0);// this is where we create the On Characterstic we had previously defined in setup().  Save this in the pointer created above, for use below
+      StatusActive=new Characteristic::StatusActive(true);
+      StatusLowBattery=new Characteristic::StatusLowBattery(1); 
+
+      LOG1(this->DataH->humidity);         
+      LOG1("Constructing Digoo successful!\n");
+    } // end constructor
+  
+  
+    void loop(){                                     // loop() method
+      if ( (millis() - DataH->updated) > activeTimeout ){
+        if (StatusActive -> getVal()) {StatusActive->setVal(false);}
+        return;
+      }
+      
+      if (DataH->isNew[1]){
+        DataH->isNew[1] = false;
+        
+        if (CurrentRelativeHumidity->getVal() != DataH->humidity) {CurrentRelativeHumidity->setVal(DataH->humidity);}
+        //CurrentRelativeHumidity->setVal(DataH->humidity);
+        if (StatusLowBattery->getVal() != DataH->batt) {StatusLowBattery->setVal(DataH->batt);}
+        
+        if ( !(StatusActive -> getVal()) ) {StatusActive->setVal(true);}
+      }
+    } // loop 
+  };
+};
